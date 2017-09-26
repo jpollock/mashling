502300ce2d19059526bb9ef68747b39731f1c179
Jeffrey Bozek <jeff.bozek@gmail.com>
2017-09-26 15:41:47 -0400

    Handle nested trigger dependencies explicitly outside of the Flogo CLI so everything gets loaded via GB.

--- lib/util/util.go
+++ lib/util/util.go
@@ -2,17 +2,22 @@
 * Copyright © 2017. TIBCO Software Inc.
 * This file is subject to the license terms contained
 * in the license file that is distributed with this file.
-*/
+ */
 package util
 
 import (
 	"encoding/json"
-	"github.com/TIBCOSoftware/flogo-cli/env"
-	ftrigger "github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
+
+	"github.com/TIBCOSoftware/flogo-cli/env"
+	ftrigger "github.com/TIBCOSoftware/flogo-lib/core/trigger"
 )
 
 func GetGithubResource(gitHubPath string, resourceFile string) ([]byte, error) {
@@ -44,11 +49,29 @@ func GetTriggerMetadata(gitHubPath string) (*ftrigger.Metadata, error) {
 
 	resourceDir := gbProject.GetVendorSrcDir()
 	triggerPath := resourceDir + "/" + gitHubPath + "/" + Gateway_Trigger_Metadata_JSON_Name
-
 	gbProject.InstallDependency(gitHubPath, "")
 	data, err := ioutil.ReadFile(triggerPath)
 	if err != nil {
-		return nil, err
+		// This hack only works for gb right now
+		gb, ok := gbProject.(*env.GbProject)
+		if !ok {
+			return nil, errors.New("this is not a GB project")
+		}
+		_, triggerErr := os.Stat(filepath.Join(gb.VendorSrcDir, triggerPath))
+		_, pathErr := os.Stat(filepath.Join(gb.VendorSrcDir, gitHubPath))
+		if !os.IsNotExist(pathErr) && os.IsNotExist(triggerErr) {
+			fmt.Fprintf(os.Stdout, "Nested trigger dependency was already loaded, manually loading higher level dependency...\n")
+			err = tweakedFlogoCliInstallDependency(gb, gitHubPath, "")
+			if err != nil {
+				return nil, err
+			}
+			data, err = ioutil.ReadFile(triggerPath)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
 	}
 	triggerMetadata := &ftrigger.Metadata{}
 	json.Unmarshal(data, triggerMetadata)
@@ -123,4 +146,33 @@ func ValidateEnvPropertySettingExpr(expression *string) (bool, *string) {
 	} else {
 		return false, &exprValue
 	}
+}
+
+// This is only called when a nested dependency has been loaded before the higher level one.
+func tweakedFlogoCliInstallDependency(e *env.GbProject, depPath string, version string) error {
+	var cmd *exec.Cmd
+
+	cwd, _ := os.Getwd()
+	defer os.Chdir(cwd)
+
+	if version == "" {
+		cmd = exec.Command("gb", "vendor", "fetch", depPath)
+	} else {
+		var tag string
+
+		if version[0] != 'v' {
+			tag = "v" + version
+		} else {
+			tag = version
+		}
+
+		cmd = exec.Command("gb", "vendor", "fetch", "-tag", tag, depPath)
+	}
+
+	os.Chdir(e.RootDir)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
 }
